da296f4719ede5979aa37a14f241f80cd0b5f0d5
Li Long <lilong988@163.com>
2015-03-31 17:39:48 +0800

    intro page view

---
+++ YKOpeningIntro/YKOpeningIntroView.h
@@ -0,0 +1,13 @@
+//
+//  YKOpeningIntroView.h
+//  YKOpeningIntro
+//
+//  Created by p2p on 15-3-31.
+//  Copyright (c) 2015年 Li Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YKOpeningIntroView : UIView
+
+@end
---
+++ YKOpeningIntro/YKOpeningIntroView.m
@@ -0,0 +1,21 @@
+//
+//  YKOpeningIntroView.m
+//  YKOpeningIntro
+//
+//  Created by p2p on 15-3-31.
+//  Copyright (c) 2015年 Li Long. All rights reserved.
+//
+
+#import "YKOpeningIntroView.h"
+
+@implementation YKOpeningIntroView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
